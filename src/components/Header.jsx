e19b8c5bcb000daffc557a63443118323ca9c89d
Geneee <jdgprofessional@gmail.com>
2025-11-10 15:05:07 +0800

    Header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   const links = [
-    { href: "visitors", label: "about" },
+    { href: "visitors", label: "visitor" },
     { href: "project", label: "projects" },
     { href: "contacts", label: "contact" },
   ];
